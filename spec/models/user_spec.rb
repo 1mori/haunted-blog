c455440c49c73b23c1e2fcc7ae2756e189444b62
yuichi.imori <yuichi.imori@classi.jp>
2024-06-27 16:47:24 +0900

    change: テストコードの文章を日本語に変更
    書籍の方法に倣って書いてみたが、日本人話者しかこのコード見ないので日本語でいいことに気づいたため

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,31 +12,26 @@ RSpec.describe User, type: :model do
     )
   end
 
-  # Eメール、パスワード、ニックネームが有効な状態であること
-  it 'is valid with a email, password, and nickname' do
+  it 'Eメール、パスワード、ニックネームが有効であること' do
     expect(subject).to be_valid
   end
 
-  # Eメールがなければ無効な状態であること
-  it 'is invalid without an email address' do
+  it 'Eメールがなければ無効であること' do
     subject.email = nil
     expect(subject).to be_invalid
   end
 
-  # パスワードがなければ無効な状態であること
-  it 'is invalid without a password' do
+  it 'パスワードがなければ無効であること' do
     subject.password = nil
     expect(subject).to be_invalid
   end
 
-  # 確認のパスワードが一致しなければ無効な状態であること
-  it 'is invalid with a mismatched password' do
+  it '確認のパスワードが一致しなければ無効であること' do
     subject.password_confirmation = 'pussword'
     expect(subject).to be_invalid
   end
 
-  # 重複したニックネームは無効な状態であること
-  it 'is invalid with a duplicate nickname' do
+  it '重複したニックネームは無効であること' do
     described_class.create!(
       email: 'test2@example.com',
       password: 'password',
