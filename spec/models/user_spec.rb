c17a27291d5313654663e90f0bb62793719007f8
yuichi.imori <yuichi.imori@classi.jp>
2024-06-27 13:21:10 +0900

    add: Userモデルのに関するテストを追加
    Eメールがない時、パスワードがない時、確認用パスワードと一致しないとき、ニックネームが重複する時に失敗するかどうかのテストを書いた

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,14 +18,32 @@ RSpec.describe User, type: :model do
   end
 
   # Eメールがなければ無効な状態であること
-  it 'is invalid without an email address'
+  it 'is invalid without an email address' do
+    subject.email = nil
+    expect(subject).to be_invalid
+  end
 
   # パスワードがなければ無効な状態であること
-  it 'is invalid without a password'
+  it 'is invalid without a password' do
+    subject.password = nil
+    expect(subject).to be_invalid
+  end
 
-  # ニックネームがなければ無効な状態であること
-  it 'is invalid without a nickname'
+  # 確認のパスワードが一致しなければ無効な状態であること
+  it 'is invalid with a mismatched password' do
+    subject.password_confirmation = 'pussword'
+    expect(subject).to be_invalid
+  end
 
-  # 重複したニックネーは無効な状態であること
-  it 'is invalid with a duplicate nickname'
+  # 重複したニックネームは無効な状態であること
+  it 'is invalid with a duplicate nickname' do
+    described_class.create!(
+      email: 'test2@example.com',
+      password: 'password',
+      password_confirmation: 'password',
+      nickname: 'testuser'
+    )
+
+    expect(subject).to be_invalid
+  end
 end
