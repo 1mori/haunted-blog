856975ae1bc4245c6edfcdf1d3cde59e1f8e0822
yuichi.imori <yuichi.imori@classi.jp>
2024-06-28 15:59:54 +0900

    delete: factory-botの設定を無効にしていた行を削除
    FactoryBotを使えるようにする

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,7 @@ module HauntedBlog
     config.time_zone = 'Tokyo'
 
     config.generators do |g|
-      g.test_framework :rspec, fixtures: false, view_specs: false, helper_specs: false, routing_specs: false
-      g.factory_bot false
+      g.test_framework :rspec, view_specs: false, helper_specs: false, routing_specs: false
     end
   end
 end
