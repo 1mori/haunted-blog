9adf8899f92483638cacf7474626fabf7f6cca8c
yuichi.imori <yuichi.imori@classi.jp>
2024-06-28 17:43:41 +0900

    add: FactoryBot.の記述を省略できる設定を追加
    必ず追加してね！くらいの圧を感じた
    
    参考URL: https://www.rubydoc.info/gems/factory_bot/file/GETTING_STARTED.md
    > RSpec
    > If you're using Rails, add the following configuration to `spec/support/factory_bot.rb` and be sure to require that file in `rails_helper.rb`:

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # FactoryBotでテストを書くときにFactorybot.を省略する設定を追加
+  config.include FactoryBot::Syntax::Methods
 end
